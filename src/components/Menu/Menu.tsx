384019f1e28115a358078bc9ce79acf533abb370
lukewalczak <lukasz.walczak.pwr@gmail.com>
2025-05-12 16:59:17 +0200

    refactor: destructure theme prop

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -198,7 +198,10 @@ const Menu = ({
   keyboardShouldPersistTaps,
 }: Props) => {
   const theme = useInternalTheme(themeOverrides);
-  const { colors, roundness } = theme;
+  const {
+    colors: { elevation: elevationColors },
+    roundness,
+  } = theme;
   const insets = useSafeAreaInsets();
   const [rendered, setRendered] = React.useState(visible);
   const [left, setLeft] = React.useState(0);
@@ -670,7 +673,7 @@ const Menu = ({
                   shadowMenuContainerStyle,
                   {
                     backgroundColor:
-                      colors.elevation[ELEVATION_LEVELS_MAP[elevation]],
+                      elevationColors[ELEVATION_LEVELS_MAP[elevation]],
                   },
                   contentStyle,
                 ]}
